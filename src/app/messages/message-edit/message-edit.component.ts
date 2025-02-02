31ab2ef2e142ef97edc28c208b73a9cf2b4bbfda
gawahlb <wah23001@byui.edu>
2025-02-01 17:58:19 -0700

    finished week 3 assignment

--- src/app/messages/message-edit/message-edit.component.ts
+++ src/app/messages/message-edit/message-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Output, ViewChild, EventEmitter } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'app-message-edit',
@@ -8,5 +9,32 @@ import { Component } from '@angular/core';
   styleUrl: './message-edit.component.css'
 })
 export class MessageEditComponent {
+  currentSender: string = 'Gabe Wahlberg';
 
+  @ViewChild('subject') subject: ElementRef;
+  @ViewChild('msgText') msgText: ElementRef;
+
+  @Output() addMessageEvent = new EventEmitter<Message>();
+
+  onSendMessage(): void {
+    const subject = this.subject.nativeElement.value;
+    const msgText = this.msgText.nativeElement.value;
+
+    const message = new Message(
+      '1',
+      subject,
+      msgText,
+      this.currentSender
+    );
+
+    this.addMessageEvent.emit(message);
+    this.onClear();
+  }
+
+
+    onClear(): void {
+      this.subject.nativeElement.value='';
+      this.msgText.nativeElement.value='';
+    }
+  
 }
--- src/app/messages/message-list/message-list.component.ts
+++ src/app/messages/message-list/message-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'app-message-list',
@@ -8,5 +9,31 @@ import { Component } from '@angular/core';
   styleUrl: './message-list.component.css'
 })
 export class MessageListComponent {
+  messages: Message[] = [
+    new Message(
+      '1',
+      'subject1',
+      'This is message 1',
+      'me'
+    ),
+
+    new Message(
+      '2',
+      'subject2',
+      'This is message 2',
+      'me'
+    ),
+
+    new Message(
+      '3',
+      'subject3',
+      'This is message 3',
+      'me'
+    )
+  ]
+
+  onAddMessage(message: Message) {
+    this.messages.push(message);
+  }
 
 }
