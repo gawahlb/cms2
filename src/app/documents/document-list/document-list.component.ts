7086049cc11a96673ec813bb479dcc6625131220
gawahlb <wah23001@byui.edu>
2025-02-06 17:31:22 -0700

    finished week 4 assignment

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'app-document-list',
@@ -8,5 +9,40 @@ import { Component } from '@angular/core';
   styleUrl: './document-list.component.css'
 })
 export class DocumentListComponent {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
 
+  documents: Document[] = [
+    new Document(
+      '1',
+      'doc1',
+      'This is doc1.',
+      'doc1 url',
+      []
+    ),
+    new Document(
+      '2',
+      'doc2',
+      'This is doc2.',
+      'doc2 url',
+      []
+    ),
+    new Document(
+      '3',
+      'doc3',
+      'This is doc3.',
+      'doc3 url',
+      []
+    ),
+    new Document(
+      '4',
+      'doc4',
+      'This is doc4.',
+      'doc4 url',
+      []
+    )
+  ]
+
+  onSelectedDocument(document: Document) {
+    this.selectedDocumentEvent.emit(document);
+  }
 }
